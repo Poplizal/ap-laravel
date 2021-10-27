3ff456d58e7162717b90cef765941da5b10d32e6
PopLizal <92744777+Poplizal@users.noreply.github.com>
2021-10-26 21:22:07 -1200

    home ui update
    home ui updating

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,23 @@
 @extends('layout');
 @section('content')
 <div class="container">
-@foreach($data as $post)
+  <h2 class="text-center py-3">Blog Sample Site</h2>
 <div class="card my-2">
-  <div class="card-header">
+  <div class="card-header text-center">
   Latest Blogs
   </div>
+@foreach($data as $post)
   <div class="card-body">
     <h5 class="card-title">{{$post->name}}</h5>
     <p class="card-text">{{$post->description}}</p>
     <a href="#" class="btn btn-primary">View</a>
   </div>
-</div>
+  <hr>
 @endforeach
 </div>
+</div>
 @endsection
+
 
 
 
